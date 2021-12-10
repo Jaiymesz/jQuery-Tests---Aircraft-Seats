7fafbf4d29b9f636343fe6a0810495e27948682c
James Parmee Morris <james@gazoop.com>
2021-12-10 17:34:53 +0100

    Task 1 Completion

--- Task 1/jQueryTest.js
+++ Task 1/jQueryTest.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
   
     $("#totalPassengers").change(function(){
 
-        if($(this).val()<0)$(this).val(0);
-        else if($(this).val()>9)$(this).val(9);
-        
+        if($(this).val() < 0){
+            $(this).val(0);
+            alert("You must enter more than 0 passengers.");
+        }
+        else if($(this).val() > 9){
+            $(this).val(9);
+            alert("You can only book 9 passengers or less.");
+        }
+
         $("#totalPrice").text($(this).val()*25);
         
     });
