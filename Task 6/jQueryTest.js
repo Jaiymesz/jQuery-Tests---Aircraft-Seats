1ac8df188669f87c1050b86e3ad31e10601fe848
James Parmee Morris <james@gazoop.com>
2021-12-11 15:05:07 +0100

    Task 6 Optimization

--- Task 6/jQueryTest.js
+++ Task 6/jQueryTest.js
@@ -3,56 +3,40 @@ $(function() {
     var ticketPrice = 25;
 
     $("#totalPassengers").on("change", function(){
-
         if($(this).val() < 0){
-
             $(this).val(0);
             alert("You must enter more than 0 passengers.");
-
         }else if($(this).val() > 9){
-
             $(this).val(9);
-            alert("You can only book 9 passengers or less.");
-            
+            alert("You can only book 9 passengers or less.");            
         }
-
         $("button.seat.selected").removeClass("selected").addClass("available");
-
-        recalculateCart();
-        
+        recalculateCart();        
     });
 
     
     $("button.seat").on("click", function(){
-
         if($(this).hasClass("available")){
             if($("button.seat.selected").length < $("#totalPassengers").val()){
                 $(this).removeClass("available").addClass("selected");
             }
-        }else if($(this).hasClass("selected"))$(this).removeClass("selected").addClass("available");
-
+        }else if($(this).hasClass("selected")){
+            $(this).removeClass("selected").addClass("available");        
+        }
         updateSeats();
-
+        recalculateCart();
     });
 
     function updateSeats(){
         var passengerI = 1;
         if( $("button.seat.selected").length==0){
-
             $("#seatList ul").html("<li>No Seats Selected</li>");
-
         }else{
-
-            recalculateCart();
             $("#seatList ul").html("");
-            
             $("button.seat.selected").each(function(i, val){   
-                if($(this).closest("ul[data-row]").data("row")==1){
-                    $("#seatList ul").append("<li>Passenger "+passengerI+": Seat "+$(this).closest("ul[data-row]").data("row")+$(this).data("seat")+" ** Extra Leg Room 15 EURO **</li>");  
-                    $("#totalPrice").text(parseInt($("#totalPrice").text())+15);
-                }else{
-                    $("#seatList ul").append("<li>Passenger "+passengerI+": Seat "+$(this).closest("ul[data-row]").data("row")+$(this).data("seat")+"</li>");  
-                }
+                let extraTxt = '';
+                if($(this).closest("ul[data-row]").data("row")==1)extraTxt = ' ** Extra Leg Room €15 **';  
+                $("#seatList ul").append("<li>Passenger "+passengerI+": Seat "+$(this).closest("ul[data-row]").data("row")+$(this).data("seat")+extraTxt+"</li>"); 
                 passengerI++;        
             });
         
@@ -61,6 +45,9 @@ $(function() {
 
     function recalculateCart(){
         $("#totalPrice").text($("#totalPassengers").val()*ticketPrice);
+        $("button.seat.selected").each(function(i, val){
+            if($(this).closest("ul[data-row]").data("row")==1)$("#totalPrice").text(parseInt($("#totalPrice").text())+15);
+        });
     }
 
     // Unavailable Seats
